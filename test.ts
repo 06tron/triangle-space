4f1f62baa66f410d188e57445dbf695986ed3d01
Matthew Richardson <97010005+06tron@users.noreply.github.com>
2025-06-04 17:43:17 -0400

    Adds test.ts

---
+++ test.ts
@@ -0,0 +1,12 @@
+const getMessage = () => "Hello World!";
+
+/**
+ * @param x - The value to increment.
+ * @returns A number greater than 'x'.
+ */
+function incr(x: number) {
+    return x + 1;
+}
+
+incr(4);
+console.log(getMessage());
